528c3b05bc948a597f5873b77611d471c81c0a77
Nick Moody <Nicholasmoodz1543@gmail.com>
2022-08-26 13:15:03 -0700

    Got back previous work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 
-function App() {
+
+const App = () => {
+  
+  //=====>STATES 
+  
+  let [books, setBooks] = useState([])
+  let [newTitle, setNewTitle] = useState('')
+  let [newAuthor, setNewAuthor] = useState('')
+  let [newDate, setNewDate] = useState('')
+  let [newPrice, setNewPrice] = useState(0)
+  let [newCover, setNewCover] = useState('')
+
+  const handleChangeDate = (event) => {
+    setNewDate(event.target.value)
+    console.log(typeof(newDate))
+  }
+  
+  const handleChangeTitle = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleChangeAuthor = (event) => {
+    setNewAuthor(event.target.value)
+  }
+
+  const handleChangeCover = (event) => {
+    setNewCover(event.target.value)
+  }
+
+  const handleChangePrice = (event) => {
+    setNewPrice(event.target.value)
+  }
+
+  const handleBookSubmission = (event) => {
+
+  }
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/books')
+    .then((response) => {
+      setBooks(response.data)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>whaddup</h1>
+      <h2>Add a New Book</h2>
+      <form onSubmit={handleBookSubmission}>
+        Title: <input type ='text' onChange={handleChangeTitle}/><br/>
+        Author: <input type ='text' onChange={handleChangeAuthor}/><br/>
+        Cover: <input type ='text' onChange={handleChangeCover}/><br/>
+        Price: <input type ='number' onChange={handleChangePrice}/><br/>
+        Date: <input type ='date' onChange={handleChangeDate}/><br/>
+        <input type='submit' value='Submit New Book'/>
+      </form>
+      {books.map((books) => {
+        return <div>
+          <h1>{books.title}</h1>
+          </div>
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
+
